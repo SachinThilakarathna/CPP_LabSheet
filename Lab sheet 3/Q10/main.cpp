35fcd1b593e324f45a042975e5e601d806cc5f43
Sachin Thilakarathna <145207063+SachinThilakarathna@users.noreply.github.com>
2023-11-12 10:45:26 +0400

    Add files via upload

---
+++ Lab sheet 3/Q10/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int x,y;
+
+cout << "x :";
+cin>> x;
+cout<< "y :";
+cin>>y;
+
+swap(x,y);
+
+cout<<"New x:"<<x<<endl
+<<"New Y:"<<y<<endl;
+return 0;
+}
